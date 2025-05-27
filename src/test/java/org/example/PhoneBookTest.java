810e1784460b65137a50961349bcfaff5e735292
dmitriy.tarasov <tds_1991@mail.ru>
2025-05-27 16:54:48 +0700

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,4 +1,18 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int count = phoneBook.add("Alice", "12345");
+        assertEquals(1, count);
+    }
+    @Test
+    public void testAddDuplicate() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Alice", "12345");
+        int count = phoneBook.add("Alice", "54321");
+    }
 }
