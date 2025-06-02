cdf466566c748613e164e6bc38cdb71a8bd40354
dmitriy.tarasov <tds_1991@mail.ru>
2025-06-02 17:07:31 +0700

    finish 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,34 +18,5 @@ public class Main {
         phoneBook.printAllNames();
     }
 
-    public static class PhoneBook {
-        private Map<String, String> contacts = new HashMap<>();
 
-        public int add(String name, String number) {
-            if (!contacts.containsKey(name)) {
-                contacts.put(name, number);
-            }
-            return contacts.size();
-        }
-
-        public String findByNumber(String number) {
-            for (Map.Entry<String, String> entry : contacts.entrySet()) {
-                if (entry.getValue().equals(number)) {
-                    return entry.getKey();
-                }
-            }
-            return null;
-        }
-
-        public String findByName(String name) {
-            return contacts.get(name);
-        }
-
-        public void printAllNames() {
-            TreeSet<String> sortedNames = new TreeSet<>(contacts.keySet());
-            for (String name : sortedNames) {
-                System.out.println(name);
-            }
-        }
-    }
 }
